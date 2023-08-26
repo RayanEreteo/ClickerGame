3ab62f55409afa7946d5fb6eb34f9e0e4e035870
RayanEreteo <rayabf5@gmail.com>
2023-08-26 22:41:53 +0200

    Mise en place spacebar et score

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import "./App.css";
+
+import SpaceBar from "./assets/spacebarlogo.svg";
 
 function App() {
-
   return (
     <div className="app">
-      <h1 className='main-title'>SpaceBar Clicker</h1>
+      <h1 className="main-title">SpaceBar Clicker</h1>
+      <div className="spacebar-container">
+        <img
+          src={SpaceBar}
+          alt="Spacebar"
+          width={"150px"}
+          style={{
+            filter:
+              "invert(100%) sepia(0%) saturate(30%) hue-rotate(206deg) brightness(105%) contrast(101%)",
+          }}
+        />
+      </div>
+      <div className="highscore-container">
+        <p id="Highscore">594</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
