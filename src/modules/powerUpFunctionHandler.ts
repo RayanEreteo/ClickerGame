4f79effa580d163c11c22e4ceda04cebe5a38b03
RayanEreteo <rayabf5@gmail.com>
2023-09-02 17:15:04 +0200

    formattage

--- src/modules/powerUpFunctionHandler.ts
+++ src/modules/powerUpFunctionHandler.ts
@@ -1,31 +1,29 @@
-let autoClickDelay = 1000
-let active = false
+let autoClickDelay = 1000;
+let active = false;
 let autoClickInterval: NodeJS.Timeout | null = null; // Define an interval variable
 
-
-function Multiplier(setMultiplier: any){
-    setMultiplier((prev: number) => prev + 1)
+function Multiplier(setMultiplier: any) {
+  setMultiplier((prev: number) => prev + 1);
 }
 
 function autoClick(setScore: any) {
-    if (active) {
-      autoClickDelay -= 50;
-      // Clear the previous interval if it exists
-      if (autoClickInterval) {
-        clearInterval(autoClickInterval);
-      }
-      // Set up a new interval with the updated delay
-      autoClickInterval = setInterval(() => {
-        setScore((prev: any) => prev += 1);
-      }, autoClickDelay);
-    } else {
-      active = true;
-      // Set up the initial interval
-      autoClickInterval = setInterval(() => {
-        setScore((prev: any) => prev += 1);
-      }, autoClickDelay);
+  if (active) {
+    autoClickDelay -= 50;
+    // Clear the previous interval if it exists
+    if (autoClickInterval) {
+      clearInterval(autoClickInterval);
     }
+    // Set up a new interval with the updated delay
+    autoClickInterval = setInterval(() => {
+      setScore((prev: any) => (prev += 1));
+    }, autoClickDelay);
+  } else {
+    active = true;
+    // Set up the initial interval
+    autoClickInterval = setInterval(() => {
+      setScore((prev: any) => (prev += 1));
+    }, autoClickDelay);
   }
-  
+}
 
-export {Multiplier, autoClick}
+export { Multiplier, autoClick };
